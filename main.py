6ca2a7664b362f6ddb70a3cab0d8f65acd94134d
SRCinVA <scitarella@hotmail.com>
2022-09-16 20:20:57 -0400

    Creating buttons in main.py

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
 from kivy.uix.widget import Widget
+
+class BoxLayoutExample(BoxLayout):
+    def __init__(slef, **kwargs):
+        super().__init__(**kwargs)  # he doesn't explain this line
+        b1 = Button(text='A')
+        b2 = Button(text='B')
+        self.add_widget(b1)
 
 class MainWidget(Widget):
     pass
