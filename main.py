37c883389aabd510cb4c6d76e49fcb282182e10b
SRCinVA <scitarella@hotmail.com>
2022-09-17 23:15:29 -0400

    small update

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 
 class BoxLayoutExample(BoxLayout):
-    def __init__(slef, **kwargs):
+    def __init__(self, **kwargs):
         super().__init__(**kwargs)  # he doesn't explain this line
         b1 = Button(text='A')
         b2 = Button(text='B')
-        self.add_widget(b1)
+        b3 = Button(text='C')
+        self.add_widget(b1) # this says that we can add a widget inside this layout.
+        self.add_widget(b2)
+        self.add_widget(b3)
 
 class MainWidget(Widget):
     pass
