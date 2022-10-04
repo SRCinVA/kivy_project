1740cc5b6e4874b178f7c9449ec866b66b41af0b
SRCinVA <scitarella@hotmail.com>
2022-10-03 20:22:58 -0400

    Stack layout, with code in both files

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from tkinter import VERTICAL
 from kivy.app import App
+from kivy.uix.stacklayout import StackLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 
+class StackLayoutExample(StackLayout):
+    def __init__(self,**kwargs): # this is the basic form for the constructor
+        super().__init__(**kwargs)
+        b = Button(text="Z")
+        self.add_widget(b)
 
 # class GridLayoutExample(GridLayout):
 #   pass
