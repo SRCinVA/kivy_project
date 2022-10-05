355026d1bbf535fc6e3d8ad5d624acc2ce227007
SRCinVA <scitarella@hotmail.com>
2022-10-04 20:10:59 -0400

    Using a loop for buttons

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import VERTICAL
 from kivy.app import App
+from kivy.metrics import dp
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.anchorlayout import AnchorLayout
@@ -10,8 +11,10 @@ from kivy.uix.widget import Widget
 class StackLayoutExample(StackLayout):
     def __init__(self,**kwargs): # this is the basic form for the constructor
         super().__init__(**kwargs)
-        b = Button(text="Z")
-        self.add_widget(b)
+        for i in range(0,10):  # you can create buttons with a loop as well
+            size = dp(100) # here, 'size' is just a local variable, not a property
+            b = Button(text=str(i+1), size_hint=(None, None), size=(size, size)) # we ran __init__ for Z, so it displayed first; then, the other buttons display.
+            self.add_widget(b)
 
 # class GridLayoutExample(GridLayout):
 #   pass
