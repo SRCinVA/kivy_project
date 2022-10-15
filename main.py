5dcd7e663526e5618788ddb0cf7d9ddae2644fdf
SRCinVA <scitarella@hotmail.com>
2022-10-14 21:50:56 -0400

    enabling and disabling buttons

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from tkinter import VERTICAL
 from kivy.app import App
 from kivy.metrics import dp
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, BooleanProperty
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.anchorlayout import AnchorLayout
@@ -12,20 +12,24 @@ from kivy.uix.widget import Widget
 
 class WidgetsExample(GridLayout):       # don't know why he's passing GridLayout
     count = 1                           # need to put the counter first
+    count_enabled = BooleanProperty(False)
     my_text = StringProperty("1")       # a custom property, for which we just want to see "1"
     
     def on_button_click(self):          # we're doing this to manage the button click
         print("Button clicked")         # to test if the method is working
-        self.count += 1                 # use self., b/c we want it to refer to the overall class, which is where we defined the variable.
-        self.my_text = str(self.count)  # if you forgot 'self,' my_text would just be a local variable and (I suspect) would *not* be applied to Widget (which is what we prefer)
+        if self.count_enabled == True:  # this has to actually be "ON" (changed to "True" down below) for 'count' to increment at all.
+            self.count += 1                 # use self., b/c we want it to refer to the overall class, which is where we defined the variable.
+            self.my_text = str(self.count)  # if you forgot 'self,' my_text would just be a local variable and (I suspect) would *not* be applied to Widget (which is what we prefer)
 
     def on_toggle_button_state(self, widget):   # remember to pass 'self' here as well
         print("toggle state: " + widget.state)  # above, 'self' refers to the Widget class; 'widget' refers to the toggle button from the kivy file (could have used 'toggle_button', as an example). We will do this frequently. 
                                                 # above, this property will tell us if the toggle has been activated. 
         if widget.state == "normal":
-            widget.text = "OFF" # with .text property, we directly change the widget's text itself
+            widget.text = "OFF"          # with .text property, we directly change the widget's text itself
+            self.count_enabled = False   # this is change the Boolean one way or another
         else:
             widget.text = "ON"
+            self.count_enabled = True    # this is change the Boolean one way or another
 
 class StackLayoutExample(StackLayout):
     def __init__(self,**kwargs): # this is the basic form for the constructor
